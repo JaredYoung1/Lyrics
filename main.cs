b7bafd77e473cca485c04eed3d6b8abb26d28b63
JaredYoung1 <143967567+JaredYoung1@users.noreply.github.com>
2023-09-04 02:34:21 +0000

    reverted back to base

--- main.cs
+++ main.cs
@@ -2,10 +2,10 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Now, this is a story all how");
-    Console.WriteLine ("My life got flipped-turned upside down");
-    Console.WriteLine ("And I'd like to take a minute");
-    Console.WriteLine ("Just sit right there");
-    Console.WriteLine ("I'll tell you how I became the prince of a town called Bel-Air");
+    Console.WriteLine ("Lyric line 1");
+    Console.WriteLine ("Lyric line 2");
+    Console.WriteLine ("Lyric line 3");
+    Console.WriteLine ("Lyric line 4");
+    
   }
 }
