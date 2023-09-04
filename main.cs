b150c2edfd913e9dd519096d4636222b58adf364
JaredYoung1 <143967567+JaredYoung1@users.noreply.github.com>
2023-09-04 02:12:27 +0000

    Unit Test #1

--- main.cs
+++ main.cs
@@ -2,9 +2,9 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Lyric line 1");
-    Console.WriteLine ("lyric line 2");
-    Console.WriteLine ("Lyric line 3");
-    Console.WriteLine ("Lyric line 4");
+    Console.WriteLine ("Oh, well imagine /");
+    Console.WriteLine ("As I'm pacing the pews in a church corridor /");
+    Console.WriteLine ("And I can't help but to hear /");
+    Console.WriteLine ("No, I can't help but to hear an exchanging of words...");
   }
 }
