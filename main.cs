2f7e897c38187f61fe630eb868c67dc4be3b6e41
JaredYoung1 <143967567+JaredYoung1@users.noreply.github.com>
2023-09-04 02:09:02 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Lyric line 1");
+    Console.WriteLine ("lyric line 2");
+    Console.WriteLine ("Lyric line 3");
+    Console.WriteLine ("Lyric line 4");
+  }
+}
